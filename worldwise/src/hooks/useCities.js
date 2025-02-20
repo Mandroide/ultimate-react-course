5de1e7138aaac6afe301daf1549a887a387138c1
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2025-02-20 05:43:08 -0400

    Change error message in useCities

--- worldwise/src/hooks/useCities.js
+++ worldwise/src/hooks/useCities.js
@@ -4,7 +4,7 @@ import {CitiesContext} from "../constants/contexts.js";
 function useCities() {
     const context = useContext(CitiesContext);
     if (context === undefined) {
-        throw new Error('useCities must be used within CitiesProvider');
+        throw new Error(`useCities must be used within ${Object.keys({CitiesContext})[0]}`);
     }
     return context;
 }
